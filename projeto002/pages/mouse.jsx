b4b79552206a78f4861b01ba838c2c015187acd5
THALES ARAUJO BARBOSA SILVA <1090481923012@fatec-srv.local>
2022-04-26 22:15:48 -0300

    aula 26/041

--- projeto002/pages/mouse.jsx
+++ projeto002/pages/mouse.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react"
+
 export default function init(){
+    const [x,setX] = useState(0)
+    const [y,setY] = useState(0)
+
+    const estilo = {height:"100vh",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}
+
+    function move(e){
+        console.log(e.clientX, e.clientY)
+    }
+
     function movimento(e){
         console.log(e)
     }
